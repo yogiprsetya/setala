c13b41de4ae5a2a541867fa3c09ff7fc961f0085
My <hello@yogiprasetya.com>
2024-06-13 20:34:12 +0700

    responsive shell

--- app/(UserZone)/_components/Header.tsx
+++ app/(UserZone)/_components/Header.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+'use client';
+
+import { Menu } from 'lucide-react';
+import { Button } from '~/components/ui/button';
+import { SheetTrigger } from '~/components/ui/sheet';
 
 export const Header = () => (
-  <header className="bg-background/95 px-6 text-foreground sticky top-0 z-30 flex h-16 w-full backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] border-b border-border/40 items-center">
+  <header className="bg-background/95 px-6 text-foreground max-sm:justify-between sticky top-0 z-30 flex h-16 w-full backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] border-b border-border/40 items-center">
+    <SheetTrigger asChild className="md:hidden">
+      <Button size="icon" variant="ghost" className="">
+        <Menu size={20} />
+      </Button>
+    </SheetTrigger>
+
     <h1>Akun</h1>
   </header>
 );
--- app/(UserZone)/layout.tsx
+++ app/(UserZone)/layout.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { Sheet } from '~/components/ui/sheet';
+import { useState } from 'react';
+import { If } from '~/components/ui/if';
+import { useIsomorphicLayoutEffect } from '~/hooks/useIsomorphicLayoutEffect';
 import { Header } from './_components/Header';
 import Sidebar from './_components/Sidebar';
 
@@ -6,14 +12,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="flex">
-      <Sidebar />
+  const [isClient, setIsClient] = useState(false);
 
-      <main className="grow">
-        <Header />
-        <div className="p-6">{children}</div>
-      </main>
-    </div>
+  useIsomorphicLayoutEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <If condition={isClient}>
+      <Sheet>
+        <div className="flex bg-background container shadow">
+          <Sidebar />
+
+          <main className="grow">
+            <Header />
+            <div className="p-6">{children}</div>
+          </main>
+        </div>
+      </Sheet>
+    </If>
   );
 }
